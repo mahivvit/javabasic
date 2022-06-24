a961aa9a30ded2764ea7b75fc95aa54cd8aa17af
mahivvit <107781815+mahivvit@users.noreply.github.com>
2022-06-24 15:32:19 +0530

    Add files via upload
    program to find reverse an array in group...

--- test.java
+++ test.java
@@ -1,39 +1,35 @@
 import java.util.Scanner;
-import java.lang.Math;
-  
-class test{
-	
-	 public static void main(String[] args){
-		 int[] x=new int[5];
-		 
-		 
-		 Scanner sc =new Scanner(System.in);
-		 System.out.println("enter array 5 digit  ");
-		 		 for(int i=0;i<5;i++){
-			 x[i]=sc.nextInt();
-		 
-		 }
-System.out.println("enter the element you want to search");
-		 
-		 int num=sc.nextInt();
-		
-		 for(int i=0;i<5;i++){
-			 if(num==x[i]){
-				 System.out.println("index:-"+i);
-				 
-				 
-			 }
-				 
-		
-		 }
-		 
-		 
+import java.util.Arrays;
 
-		 
-		 
-		 
-		 
+class test{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter size of array");
+		int n=sc.nextInt();
+		System.out.println("enter index of group ");
+		int k=sc.nextInt();
+		int[] arr=new int[n];
+		int[] arr1=new int[n];
+		int temp=0;
+		System.out.println("enter element:-");
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+			
+		}
+		System.out.println("group1 reverse= ");
 		
+		for(int i=k;i>=0;i--){
 	
-	}
+			System.out.println(arr[i]);
+		}
+			
+			System.out.println("group2 reverse= ");
+		
+		
+		for(int i=n-1;i>k;i--){
+			System.out.println(arr[i]);
+		
+		}
+		}
+	
 }
